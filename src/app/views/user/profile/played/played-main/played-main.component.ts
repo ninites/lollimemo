7ee3d2a40516ac3977a8e881dd17d4f752884645
Romain <rmnheo@gmail.com>
2021-07-24 03:06:10 +0200

    query working filter

--- src/app/views/user/profile/played/played-main/played-main.component.ts
+++ src/app/views/user/profile/played/played-main/played-main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RequestService } from 'src/app/core/services/request/request.service';
 
 @Component({
@@ -7,9 +8,27 @@ import { RequestService } from 'src/app/core/services/request/request.service';
   styleUrls: ['./played-main.component.scss'],
 })
 export class PlayedMainComponent implements OnInit {
-  constructor(private request: RequestService) {}
+  constructor(
+    private request: RequestService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.request.get('games').subscribe(console.log);
+    this.route.queryParams.subscribe((query) => {
+      let queryStrings = '?';
+      for (const key in query) {
+        queryStrings += key + '=' + query[key] + '&';
+      }
+      this.request.get('games/' + queryStrings).subscribe(console.log);
+    });
+  }
+
+  filter(name: string, type: string) {
+    const params = {
+      type: type,
+      opponent: name,
+    };
+    this.router.navigate(['/user/profile/played'], { queryParams: params });
   }
 }
--- src/app/views/user/profile/played/played.module.ts
+++ src/app/views/user/profile/played/played.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { PlayedRoutingModule } from './played-routing.module';
 import { PlayedMainComponent } from './played-main/played-main.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
   declarations: [PlayedMainComponent],
   imports: [
     CommonModule,
+    SharedModule,
     PlayedRoutingModule
   ]
 })
