9edd0d0630cf16d553e8cc060f85242ebf6f37dc
Romain <rmnheo@gmail.com>
2021-08-06 11:19:42 +0200

    WIP [skip ci]

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  proxy : "/api/"
+  proxy: '/api/',
+  googleSearch: 'AIzaSyD9PlVsTVjgDTBL00EHLePsRJQ3EsNQBNg',
+  googleSearchCx: '0621a687ce2741caf',
+  googleSearchUrl: 'https://customsearch.googleapis.com/customsearch/v1',
 };
